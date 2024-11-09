159ebdc6811601d1a4cd0ff7c904f9db3a3da623
gabreel05 <gabrielgoncalvesmachado@hotmail.com>
2024-11-08 21:54:17 -0300

    Adding MySQL dep

--- knex.ts
+++ knex.ts
@@ -1,13 +1,13 @@
 import knex from 'knex';
 
 const db = knex({
-  client: 'pg',
+  client: 'mysql2',
   connection: {
-    host: 'localhost',
-    port: 5432,
-    user: 'acme',
-    password: 'acme',
-    database: 'acme',
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   },
   useNullAsDefault: true,
 });
--- knexfile.js
+++ knexfile.js
@@ -5,13 +5,13 @@
  */
 module.exports = {
   development: {
-    client: 'pg',
+    client: 'mysql2',
     connection: {
-      host: 'localhost',
-      port: 5432,
-      user: 'acme',
-      password: 'acme',
-      database: 'acme',
+      host: process.env.DB_HOST,
+      port: 3306,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
     },
     migrations: {
       directory: './migrations',
