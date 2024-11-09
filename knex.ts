6c9032c965168de700e00195b7855b0ce54b39d9
gabreel05 <gabrielgoncalvesmachado@hotmail.com>
2024-11-09 15:47:51 -0300

    Updating database

--- knex.ts
+++ knex.ts
@@ -2,13 +2,7 @@ import knex from 'knex';
 
 const db = knex({
   client: 'mysql2',
-  connection: {
-    host: process.env.DB_HOST,
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  },
+  connection: process.env.DB_URL,
   useNullAsDefault: true,
 });
 
